05625d7f00b61a4624d20f38f7ed60af34486fdd
Mitchel Severe <truth531@gmail.com>
2017-06-30 00:30:53 -0400

    Added chessboard alternating lines

--- 02/chessboard.js
+++ 02/chessboard.js
@@ -2,12 +2,12 @@ var boardSize = 8;
 var position = '';
 
 for (x = 0; x < boardSize; x++) {
-    for (y = 0; y <= boardSize; y++) {
-        if (y % 2 === 0) {
+    for (y = 0; y < boardSize; y++) {
+        if ((x + y) % 2 === 0) {
             position += ' ';
         } else {
             position += '#';
-        }
+        } 
     }
     position += '\n';
 }
